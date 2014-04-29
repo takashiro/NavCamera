741812f86893b4022a28a80f4c2f295ca01dfdfa
Takashiro <takashiro@qq.com>
2014-04-29 13:43:43 +0800

    根据上次更新时间决定是否显示操作简介
    Signed-off-by: Takashiro <takashiro@qq.com>

--- src/com/nmlzju/navcamera/BootActivity.java
+++ src/com/nmlzju/navcamera/BootActivity.java
@@ -1,68 +1,65 @@
 package com.nmlzju.navcamera;
 
-import java.lang.ref.WeakReference;
-
 import com.nmlzju.navcamera.R;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.preference.PreferenceManager;
 
 public class BootActivity extends Activity {
 	/** Called when the APP starts. */
 
-	private static final int Version = 1;
-	
-	private static class ActivityHandler extends Handler{
-		private WeakReference<Activity> parent = null;
-		
-		public ActivityHandler(Activity activity){
-			parent = new WeakReference<Activity>(activity);
-		}
-		
-		public void handleMessage(Message msg){
-			Activity activity = parent.get();
-			SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(activity);
-			Intent intent = null;
-			if(settings.getInt("firstRun", 0) != Version){
-				intent = new Intent(activity, GuideActivity.class);
-				Editor editor = settings.edit();
-				editor.putInt("firstRun", Version);
-				editor.commit();
-			}else{
-				intent = new Intent(activity, CameraActivity.class);
-			}
-			activity.startActivity(intent);
-			activity.finish();
-		}
-	}
-	private Handler activityHandler = null;
+	private Handler activityHandler = new Handler();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.boot);
 		
-		activityHandler = new ActivityHandler(this);
-
-		new Thread(new Runnable() {
+		activityHandler.postDelayed(new Runnable(){
 
 			@Override
 			public void run() {
+				PackageManager manager = getPackageManager();
+				PackageInfo info = null;
 				try {
-					Thread.sleep(500);
-					activityHandler.sendEmptyMessage(0);
-				} catch (InterruptedException e) {
+					info = manager.getPackageInfo(getPackageName(), 0);
+				} catch (NameNotFoundException e) {
+					e.printStackTrace();
+					return;
+				}
+				
+				SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(BootActivity.this);
+				Intent intent = null;
+				
+				long lastUpdateTime = 0;
+				try{
+					lastUpdateTime = settings.getLong("lastUpdateTime", 0);
+				}catch(ClassCastException e){
 					e.printStackTrace();
 				}
+				
+				if(lastUpdateTime != info.lastUpdateTime){
+					intent = new Intent(BootActivity.this, GuideActivity.class);
+					Editor editor = settings.edit();
+					editor.putLong("lastUpdateTime", info.lastUpdateTime);
+					editor.commit();
+				}else{
+					intent = new Intent(BootActivity.this, CameraActivity.class);
+				}
+				
+				startActivity(intent);
+				finish();
 			}
-
-		}).start();
+			
+		}, 500);
 	}
 
 }
